be1b891690a09dc4220c61b2feb2d95c8e9c920d
Henrique <henriquesilva254@gmail.com>
2023-07-10 16:57:18 -0300

    myWallet-front deploy vercel

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -3,11 +3,11 @@ import axios from "axios";
 
 
 function signUp(body){
-    const promisse = axios.post(`${import.meta.env.VITE_API_URL}sign-up`, body)
+    const promisse = axios.post(`${import.meta.env.VITE_API_URL}/sign-up`, body)
     return promisse
 }
 function signIn(body){
-    const promisse = axios.post(`${import.meta.env.VITE_API_URL}sign-in`, body)
+    const promisse = axios.post(`${import.meta.env.VITE_API_URL}/sign-in`, body)
     return promisse 
 }
 
